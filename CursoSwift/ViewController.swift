f1b5f9efe992e55056962da197a6ee0fdd1d2d53
Marco Cumbicos <mcumbicos@macbook-pro-de-marco.local>
2016-03-09 15:13:55 -0500

    Initial Commit

---
+++ CursoSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CursoSwift
+//
+//  Created by Marco Cumbicos on 9/03/16.
+//  Copyright © 2016 Marco Cumbicos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
